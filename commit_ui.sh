6a9924606ddcad3929133d667d6da60d476d0be1
Hidden <zzdhidden@gmail.com>
2010-09-14 16:34:47 +0800

    UP

--- commit_ui.sh
+++ commit_ui.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 cd uchome
+git add static
 git commit static -m "Update UI"
 
 cd ../discuz
+git add static
 git commit static -m "Update UI"
 
 cd ../discuzX
+git add static
+git commit static -m "Update UI"
+
+cd ../phpwind
+git add static
 git commit static -m "Update UI"
